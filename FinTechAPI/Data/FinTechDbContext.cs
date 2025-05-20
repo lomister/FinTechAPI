beb435727376c840087f71c4f308164920342738
rudansd <rudansd@stahls.com>
2025-05-20 21:49:18 +0300

    commit

---
+++ FinTechAPI/Data/FinTechDbContext.cs
@@ -0,0 +1,42 @@
+﻿// FinTechDbContext.cs
+using FinTechAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinTechAPI.Data
+{
+    public class FinTechDbContext : IdentityDbContext<User>
+    {
+        public FinTechDbContext(DbContextOptions<FinTechDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Configure relationships and constraints
+            modelBuilder.Entity<Account>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Accounts)
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+                
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.Account)
+                .WithMany(a => a.Transactions)
+                .HasForeignKey(t => t.AccountId)
+                .OnDelete(DeleteBehavior.Cascade);
+                
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Transactions)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.NoAction); // To avoid circular cascade delete
+        }
+    }
+}
